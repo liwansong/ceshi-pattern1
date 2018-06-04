85f18eb56c8e6338f67c8dd6d5f2a649695c630e
liwansong <615985141@qq.com>
2018-06-04 20:45:41 +0800

    分支提交

--- ceshi-pattern1/src/com/cnpc/ceshi/CopyOfTest.java
+++ ceshi-pattern1/src/com/cnpc/ceshi/CopyOfTest.java
@@ -6,6 +6,7 @@ public class CopyOfTest {
 	 public void say(CopyOfTest obj){
 		 a=obj;
 		 System.out.println(a);
+		 System.out.println("分支内容哦");
 	 }
 	 public static void main(String[] args){
 		 CopyOfTest test=new CopyOfTest();
